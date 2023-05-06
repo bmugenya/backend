c206c502e09e9166513f16f7e96a18486af4afd5
bmugenya <bmugenya26@gmail.com>
2023-05-07 00:08:44 +0300

    update

--- Flask/app/api/v1/model/model.py
+++
@@ -1,9 +0,0 @@
-
-class Model():
-
-    def __init__(self):
-        pass
-
-    def share(self,email,story):
-        pass
-
--- Flask/run.py
+++ Flask/run.py
@@ -1,6 +1,9 @@
-from app import create_app
+import os
+from api import create_app
 
-app = create_app()
+config = os.getenv('APP_SETTINGS')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app = create_app(config)
+
+if __name__ == "__main__":
+    app.run()
